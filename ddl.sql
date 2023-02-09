2676b2385a92766afdd51f583a42c863ab8202f7
Швейников Андрей Борисович <ShveynikovAB@st.tech>
2023-02-09 09:29:24 +0300

    v.0

---
+++ ddl.sql
@@ -0,0 +1,105 @@
+CREATE TABLE dbo.vw_optimatica_dealers
+(
+    date date,
+    month_num numeric(18,0),
+    year_n numeric(18,0),
+    dealer_name varchar(6000),
+    grouping varchar(31),
+    model varchar(6000),
+    media varchar(6000),
+    plan_price float,
+    "avg plan price" float,
+    "avg fact price" float
+);
+
+CREATE TABLE dbo.vw_total_dealers
+(
+    ym_s_date date,
+    ym_s_clientID numeric(30,0),
+    num_user int,
+    visitID numeric(30,0),
+    dateTime_visit timestamp,
+    grouping varchar(2000),
+    source_detail varchar(4000),
+    ym_s_startURL_domain text,
+    Дилер varchar(58),
+    Оптиматика_Дилер varchar(72),
+    "Федеральный округ" varchar(65),
+    Город varchar(34),
+    source varchar(57),
+    source_2 varchar(8000),
+    medium varchar(4000),
+    campaign varchar(4000),
+    model varchar(41),
+    content varchar(4000),
+    cid varchar(4000),
+    pageViews int,
+    visitDuration int,
+    bounce int,
+    count_visit int,
+    count_goals int
+);
+
+
+CREATE TABLE dbo.vw_total_dealers_all
+(
+    ym_s_date date,
+    ym_s_clientID numeric(30,0),
+    num_user int,
+    visitID numeric(30,0),
+    dateTime_visit timestamp,
+    grouping varchar(2000),
+    source_detail varchar(4000),
+    source varchar(57),
+    content varchar(4000),
+    cid varchar(4000),
+    pageViews int,
+    visitDuration int,
+    bounce int,
+    count_visit int,
+    count_goals int
+);
+
+
+CREATE TABLE dbo.vw_worklists_models_dealers
+(
+    "ID Сессии в Я.Метрика (заявки)" varchar(30),
+    "Домен (заявки)" varchar(500),
+    "Дата события CRM" date,
+    Заявки int,
+    "Рабочие листы" int,
+    Договора int,
+    "Выданные ТС" int,
+    "Домен метрики" text,
+    grouping varchar(2000),
+    source_detail varchar(4000),
+    medium varchar(4000),
+    source varchar(57),
+    model varchar(41),
+    Дилер varchar(58),
+    "Федеральный округ" varchar(65),
+    Город varchar(34),
+    ym_s_visitID numeric(30,0),
+    ym_s_counterID numeric(30,0),
+    cid int,
+    format_ads varchar(33)
+);
+
+
+CREATE TABLE dbo.vw_worklists_models_dealers_all
+(
+    "ID Сессии в Я.Метрика (заявки)" varchar(30),
+    "Дата события CRM" date,
+    Заявки int,
+    "Рабочие листы" int,
+    Договора int,
+    "Выданные ТС" int,
+    grouping varchar(2000),
+    source_detail varchar(4000),
+    medium varchar(4000),
+    source varchar(57),
+    ym_s_visitID numeric(30,0),
+    ym_s_counterID numeric(30,0),
+    cid int,
+    format_ads varchar(33)
+);
---
+++ web_analytics_transfer_dag.py
@@ -0,0 +1,120 @@
+import pandas as pd
+import sqlalchemy as sa
+from urllib.parse import quote
+import datetime as dt
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.task_group import TaskGroup
+from airflow.hooks.base import BaseHook
+from airflow.operators.dummy import DummyOperator
+from airflow.utils.dates import days_ago
+from airflow.contrib.operators.vertica_operator import VerticaOperator
+from airflow.operators.python import BranchPythonOperator
+from airflow.models import TaskInstance
+
+
+external_bi_con = BaseHook.get_connection('ext_pbi')
+external_bi_username = external_bi_con.login
+external_bi_password = quote(external_bi_con.password)
+external_bi_host = external_bi_con.host
+external_bi_db = 'sttt_data_marts'
+external_bi_str = fr'mssql://{external_bi_username}:{external_bi_password}@{external_bi_host}/{external_bi_db}?driver=ODBC Driver 18 for SQL Server&TrustServerCertificate=yes'
+external_bi_engine = sa.create_engine(eng_str)
+
+dwh_con = BaseHook.get_connection('vertica')
+ps = quote(dwh_con.password)
+dwh_engine = sa.create_engine(
+    f'vertica+vertica_python://{dwh_con.login}:{ps}@{dwh_con.host}:{dwh_con.port}/sttgaz'
+)
+
+
+def extract(table_name, query_part):
+    return pd.read_sql_query(
+        f"""SELECT * FROM sttgaz.{table_name}""" + query_part,
+        dwh_engine
+    )
+
+
+def transform(data):
+    return data
+
+
+def load(data, table_name, query_part):
+
+    pd.read_sql_query(
+        f"""DELETE FROM {table_name}""" + query_part,
+        vexternal_bi_engine,
+    )
+
+    data.to_sql(
+        table_name,
+        external_bi_engine,
+        schema='dbo',
+        if_exists='append',
+        index=False,
+    )        
+
+
+def etl(table_name, **context):
+    if not context['prev_execution_date_success']:
+        query_part = ''
+    elif table_name in ('vw_optimatica_dealers',
+                        'vw_total_dealers',
+                        'vw_total_dealers_all'):
+        query_part = f"""WHERE ym_s_date >= {context['execution_date'] - dt.timedelta(month=2)}"""
+    elif table_name in ('vw_worklists_models_dealers',
+                        'vw_worklists_models_dealers_all'):
+        query_part = f"""WHERE "Дата события CRM" >= {context['execution_date'] - dt.timedelta(month=2)}"""
+
+    load(
+        transform(
+            extract(table_name, query_part)
+        ),
+        table_name,
+        query_part
+    )
+
+
+default_args = {
+    'owner': 'Швейников Андрей',
+    'email': ['shveynikovab@st.tech'],
+    'retries': 4,
+    'retry_delay': dt.timedelta(minutes=30),
+}
+with DAG(
+        'web_analytics_etl',
+        default_args=default_args,
+        description='Перемещение данных по веб-аналитике из хранилища во внешнюю BI-систему.',
+        start_date=days_ago(1),
+        schedule_interval='@daily',
+        catchup=True,
+        max_active_runs=1
+) as dag:
+
+    start = DummyOperator(task_id='Начало')
+
+    with TaskGroup('ETL') as tg:
+
+        tasks = []
+        data_types = (
+            'vw_optimatica_dealers',
+            'vw_total_dealers',
+            'vw_total_dealers_all',
+            'vw_worklists_models_dealers',
+            'vw_worklists_models_dealers_all',
+        )
+        for data_type in data_types:
+            tasks.append(
+                PythonOperator(
+                    task_id=f'Перемещение_данных_{data_type}',
+                    python_callable=etl,
+                    op_kwargs={'table_name': data_type},
+                )
+            )
+
+        tasks
+
+    end = DummyOperator(task_id='Конец')
+
+    start >> tg >> end
